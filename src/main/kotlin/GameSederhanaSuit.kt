ac9ca266ff8d291adb0fe91a72bba83cde78f6e4
MasKoding <masdidi00@gmail.com>
2023-08-02 13:17:12 +0700

    Update Game Class

--- src/main/kotlin/GameSederhanaSuit.kt
+++ src/main/kotlin/GameSederhanaSuit.kt
@@ -27,6 +27,32 @@ class GameSederhanaSuit {
             }
         }
     }
+    fun suit(){
+        print("Masukkan Pilihan Anda (B/G/K):")
+        opsi1 = readLine()!!.toString()
+        // fungsi shuffled adalah untuk merubah urutan array secara random
+        // ambil index 0
+        var computerOpsi = opsi2.shuffled()[0]
+        println("Computer Memilih : $computerOpsi")
+
+        when(opsi1){
+            "B"->when(computerOpsi){
+                "G"->{
+                    status="Win"
+                    increamentScore()
+                }
+                "B"->{
+                    status="Draw"
+                }
+                "K"->{
+                    status="Lose"
+                    decreamentScore()
+                }
+            }
+        }
+
+    }
+
 
 
 }
