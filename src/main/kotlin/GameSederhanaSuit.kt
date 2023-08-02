d1d8d86748cdb0e38f6ef7f99454bec6b62d71da
MasKoding <masdidi00@gmail.com>
2023-08-02 13:11:14 +0700

    Update Game Class

--- src/main/kotlin/GameSederhanaSuit.kt
+++ src/main/kotlin/GameSederhanaSuit.kt
@@ -3,6 +3,7 @@ class GameSederhanaSuit {
     var score:Int = 0
     var opsi1:String=""
     var opsi2 = listOf("B","G","K")
+    var status:String=""
 
     constructor(paramPlayerName:String){
         playerName=paramPlayerName
@@ -15,5 +16,17 @@ class GameSederhanaSuit {
         score-=10
         println("Score: $score")
     }
+    fun gameStart(){
+        // game akan jalan terus ketika di mulai
+        while (true){
+            print("Apakah Anda ingin bermain (Y/N):")
+            var start:String= readLine()!!.toString()
+            when(start){
+                "Y"->suit()
+                else->break
+            }
+        }
+    }
+
 
 }
